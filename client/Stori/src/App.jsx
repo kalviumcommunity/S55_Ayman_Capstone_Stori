ded8a6ce5cf26567c634aa35ec231df934993824
Ayman Velani <secondayman@gmail.com>
2024-04-08 15:41:56 +0530

    pages-made

--- client/Stori/src/App.jsx
+++ client/Stori/src/App.jsx
@@ -1,10 +1,17 @@
 import Home from './Components/Home/Home'
+import { Route,Routes } from 'react-router-dom'
+import Login from './Components/Login/Login'
+import Signup from './Components/Signup/Signup'
 
 function App() {
   
   return (
     <>
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/signup' element = {<Signup/>} />
+        <Route path='/login' element = {<Login/>} />
+      </Routes>
     </>
   )
 }
--- client/Stori/src/Components/Home/Home.jsx
+++ client/Stori/src/Components/Home/Home.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import '../Home/Home.css';
 import storiLogo from '../../assets/Stori.png';
+import { useNavigate } from 'react-router-dom'
+
 
 function Home() {
 
+    const navigate = useNavigate();
+
     return (
-        <div className="bg">
-            <img src={storiLogo} alt="Stori-logo" id='stori-logo' />
-            <h1 className="stori-headline">Uniting Voices, Crafting Tales</h1>
-            <div className="content">
-                <p>A platform  not only for the writers of the world , but for the thinkers and fighters of the daily circus of society. Welcome to safe-space every shrink tries to boast about!</p>
-                <p>Join the journey , share your Stori.</p>
+        <div className="main-div">
+            <div className="bg">
+                <img src={storiLogo} alt="Stori-logo" id='stori-logo' />
+                <h1 className="stori-headline">Uniting Voices, Crafting Tales</h1>
+                <div className="content">
+                    <p>A platform  not only for the writers of the world , but for the thinkers and fighters of the daily circus of society. Welcome to safe-space every shrink tries to boast about!</p>
+                    <p>Join the journey , share your Stori.</p>
+                </div>
+                <div className="buttons">
+                    <button id="cred" onClick={() => { navigate('/signup') }}>Sign up</button>
+                    <button id="cred" onClick={() => { navigate('/login') }}>Login</button>
+                </div>
             </div>
-            <div className="buttons">
-                <button id="cred">Sign up</button>
-                <button id="cred">Login</button>
-            </div>
+
         </div>
+
     )
 }
 
