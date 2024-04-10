97060c7d81fa5c6f7c2b0e6ebc853c13c421f0c3
Ayman Velani <secondayman@gmail.com>
2024-04-10 13:54:00 +0530

    codium changes made

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
 
 app.get('/get',(req,res)=>{
     res.send("Working")
 })
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT, ()=>{
     console.log('all good')
 })
